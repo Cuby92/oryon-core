fdd4e675784fda66a5e31dfd37a473f79ba0dfa5
Cuby92 <jakub.bartul@hotmail.com>
2026-07-17 14:41:11 +0200

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "tsup";
+import sassPlugin from "esbuild-plugin-sass";
 
 export default defineConfig({
-  entry: ["src/index.ts"],  // The library's entry point
+  entry: {
+    index: "src/index.ts",
+    types: "src/types/index.ts",
+    functions: "src/functions/index.ts"
+  },
   format: ["cjs", "esm"],   // Output both CommonJS and ESModules
   dts: true,                // Generate declaration files (.d.ts)
   splitting: false,
   sourcemap: true,          // Source maps for debugging
   clean: true,              // Rebuild clean dist folder every time
   external: ["react"],      // Avoid bundling React into your library
+  plugins: [sassPlugin()]
 });
